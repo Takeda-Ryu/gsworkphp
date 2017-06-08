be77d14a1f1a65a88f7bac62d8b7439d583ac165
武田龍 <takedaryu@takedaryuu-no-MacBook-Pro.local>
2017-06-08 11:47:57 +0900

    サンプルをコミット

---
+++ samplePhp/test.php
@@ -0,0 +1,27 @@
+<?php
+
+$num = rand(1,5);
+
+if($num==1){
+  echo "あたり";
+}
+
+if($num==2){
+  echo "ハズレ";
+}
+
+if($num==3){
+  echo "まぁまぁ";
+}
+
+if($num==4){
+  echo "うーん";
+}
+
+if ($num==5) {
+  echo "最悪";
+}
+
+
+
+ ?>
---
+++ samplePhp/testar.php
@@ -0,0 +1,9 @@
+<?php
+
+$str = "PHP4,PHP5,PHP6";
+
+var_dump($str);
+
+// echo $str;
+
+?>
